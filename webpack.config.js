5cf3cf142e8bb855173b82b96d3da9f4abe6a53d
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-04-08 20:03:57 +0200

    chore: add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js|.ts$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
     ],
   },
